5eb097c5c5577835a78756df0e685e7272c6e75e
Passionate Engineer Ryoju <ryoju.builder@gmail.com>
2019-02-04 20:04:26 +0900

    Unsupport ssl

--- server/websocket/main.js
+++ server/websocket/main.js
@@ -2,25 +2,8 @@ const fs = require('fs')
 const moment = require('moment')
 const request = require('request')
 const sha512 = require('js-sha512').sha512
-const https = require('https')
-
-const keys = {
-  key : './certs/key.key',
-  cert : './certs/cert.pem',
-  chain : './certs/chain.pem',
-  fullchain : "./certs/fullchain.pem"
-}
-
-let server
-try {
-  server = http.createServer({
-    key: fs.readFileSync(keys.key),
-    cert: [fs.readFileSync(keys.cert)],
-    ca: [fs.readFileSync(keys.chain), fs.readFileSync(keys.fullchain)]
-  })
-} catch (e) {
-  server = http.createServer()
-}
+const http = require('http')
+const server = http.createServer()
 
 // *************************
 // Functions
